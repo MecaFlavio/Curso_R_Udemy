4180f71f7853c564eddabcf39165bcdc9df05167
Flavio A. Pereira <flavio.metodista@hotmail.com>
2022-02-06 12:54:17 -0300

    Exercícios de Cálculos Matemáticos com R
    Exercícios realizados no curso de R no Udemy. Simples exercícios de cálculos matemáticos.

---
+++ exercicios calculos matematicos.R
@@ -0,0 +1,16 @@
+# 1. Use o R Studio para extrair a raíz quadrada de 85.
+
+sqrt(85)
+
+# 2. Calcule a seguinte equação usando o R Studio 15/(10²).(Nota: para calcular 
+# um número ao quadrado pode usar n**2 onde n é o numero que quer colocar ao quadrado):
+
+15/(10**2)
+
+# 3. Calcule a seguinte equação usando o R Studio: (215 + log(100))/(e^100).
+
+(215 + log(100))/(exp(100))
+
+# 4. Use o R para calcular o logaritmo natural (base e) do número 24.
+
+log(24)
